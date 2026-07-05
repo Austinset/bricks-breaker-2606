9584ed26297860ce70828b53af60c4f5236d9dbd
Austinset <ajset1@gmail.com>
2026-07-05 18:42:27 -0400

    declaration of win state for task 5 (later) and a hittracker template.

--- Game.h
+++ Game.h
@@ -9,10 +9,13 @@ class Game
 	Box paddle;
 
 	// TODO #1 - Instead of storing 1 brick, store a vector of bricks (by value)
-	std::vector<Box> brick;
+	std::vector<Box> bricks;
 
 	//Potential tracker for hits for task 5
+	std::vector<int> brickHits;
 	//A win/lose state
+	bool gameOver;
+	bool playerWin;
 
 public:
 	Game();
